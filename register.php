08462a91ab72197f290ebe75b25007021882e5b9
melissajmcmahon <melissajmcmahon@email.arizona.edu>
2018-05-06 19:49:52 -0700

    Update register.php

--- register.php
+++ register.php
@@ -1,3 +1,5 @@
+<!-- This page contains the HTML code for the register page of the website along with the javascript/ajax call to register the
+user into the database.-->
 <?php
 session_start();
 ?>
@@ -20,26 +22,30 @@ Password: <input type = "password" id = "PW" placeholder = "Password" required p
 </div>
 </div>
 <script>
+// registerMe calls an ajax function to do the necessary error checking and database updating in order to register a user.
 function registerMe() {
+	// UN will hold the users desired Username as a string.
 	var UN = document.getElementById("UN").value;
+	// PW will hold the users desired Password as a string.
 	var PW = document.getElementById("PW").value;
+	// check is a div that will allow for error messages to show up.
 	var check = document.getElementById("check");
 	 if (document.getElementById('UN').validity.valid && document.getElementById('PW').validity.valid ){
-		 //alert("valid")
-		 
+		 // checking if all fields were submitted and with the correct pattern.
 		var anObj = new XMLHttpRequest();
 		anObj.open("GET", "controller.php?UN=" + UN+"&PW="+PW, true);
 		anObj.send();
-
  		anObj.onreadystatechange = function () {
 			if (anObj.readyState == 4 && anObj.status == 200) {
 				var curr = anObj.responseText;
-				if(curr == "already here"){
-				
+				// if the response text is 'already here' then this username/account already exists, don't want
+				// to create duplicate accounts in the database.
+				if(curr == "already here"){				
 					check.innerHTML = "<br>This account already exists.";
-				//alert(curr);
 				}
 				else{
+					//otherwise, controller.php will have registered the account into the database, so send
+					// the user to the home page.
 					window.location.href = 'quotes.php';
 				}
 		
@@ -47,6 +53,7 @@ function registerMe() {
 		}
 	 }
 	 else{
+		 // if the user did not fill out the form to the desired pattern, or left fields blank, throw an error.
 		 check.innerHTML = "<br>Please Fill out both fields <br> Username must be length 4 <br> Password must be length 6";
 	 }
 }
