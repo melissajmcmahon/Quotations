7dad6a0a979678d200df18830f0cda12a0013080
melissajmcmahon <melissajmcmahon@email.arizona.edu>
2018-05-06 17:28:58 -0700

    Update DatabaseAdapter.php

--- DatabaseAdapter.php
+++ DatabaseAdapter.php
@@ -17,52 +17,59 @@ class DatabaseAdaptor {
             exit ();
         }
     }
+    //this function gets an array of quotes/authors from the database as long as its not flagged, and in order of
+    // rating.
     public function getAllQuotes() {
         $stmt = $this->DB->prepare( 'SELECT * FROM quotations where flagged = 0 ORDER BY rating DESC, added DESC');
         $stmt->execute ();
         return $stmt->fetchAll ( PDO::FETCH_ASSOC );
     }
-
+    //this function changes the rating in the database given a positive or negative 1 and the quote id.
     public function changeRating($quoteId,$addOrSub){
             $stmt = $this->DB->prepare("update quotations set rating = rating+". $addOrSub ." where id = ". $quoteId );
             $stmt->execute ();
     }
+    // this adds a quote to the database given the quote and its id.
     public function addThisQuote($quote,$author){
         $stmt = $this->DB->prepare( "insert into quotations(quote, author, rating, flagged,added) values('".$quote."', '".$author."', 0, 0,Now())");
         $stmt->execute ();
-        //return $stmt->fetchAll ( PDO::FETCH_ASSOC );
     }
+    // this function checks whether or not the username exists in the database.
     public function checkMe($UN){
         $stmt = $this->DB->prepare('SELECT username from users where username ="' .$UN.'"');
         $stmt->execute ();
         return $stmt->fetchAll ( PDO::FETCH_ASSOC );
     }
+    // this function adds a new quote and hashed password to the database.
     public function registerMe($UN,$PWHASH){
         $stmt = $this->DB->prepare('insert into users(username, hash) values("'.$UN.'","'.$PWHASH.'")');
         $stmt->execute ();
     }
+    //this function returns the hash password in order to check the if the login attempt is valid.
     public function getHash($UN){
-        //select hash password.
         $stmt = $this->DB->prepare('select hash from users where username = "'.$UN.'";');
         $stmt->execute ();
         return $stmt->fetchAll ( PDO::FETCH_ASSOC );
     }
+    // this function changes the flagged boolean in the database given a quote id.
     public function flagThis($idF){
         $stmt = $this->DB->prepare("update quotations set flagged = 1 where id = ".$idF);
         $stmt->execute ();
     }
+    //this function returns a users id based off of the username.
     public function getId($UN){
         $stmt = $this->DB->prepare("select id from users where username = '".$UN."'");
         $stmt->execute ();
         return $stmt->fetchAll ( PDO::FETCH_ASSOC );
     }
+    //this function goes through the database and changes all 'flagged' booleans to 0.
     public function unflagALL(){
         $stmt = $this->DB->prepare("update quotations set flagged = 0");
         $stmt->execute ();
     }
 }
 $theDBA = new DatabaseAdaptor ();
-// Test code that must be deleted later
+// Test code for some of the functions.
 //$arr = $theDBA->getAllQuotes ( '*' );
 //print_r ( $arr );
 //$arr = $theDBA->changeRating ( 1 ,-1 );
